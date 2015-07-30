038da233d90cadabf534967540e79024102d8b48
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2015-07-30 06:56:57 -0400

    Add simple checkbox component

---
+++ __tests__/InputCheckboxSpec.js
@@ -0,0 +1,25 @@
+// jest.autoMockOff();
+jest.dontMock('../src/InputCheckbox');
+
+describe('InputCheckboxSpec.js', function() {
+
+	const React = require('react/addons');
+	const InputCheckbox = require('../src/InputCheckbox');
+	const TestUtils = React.addons.TestUtils;
+
+	afterEach(function(done) {
+        React.unmountComponentAtNode(document.body);
+        document.body.innerHTML = '';
+        setTimeout(done);
+    });
+
+	it('should be defined', function() {
+		expect(InputCheckbox).toBeDefined();
+	});
+
+	it('should be a valid react component', function() {
+		const checkbox = TestUtils.renderIntoDocument(<InputCheckbox />);
+		expect(TestUtils.isCompositeComponent(checkbox)).toBeTruthy();
+	});
+
+});
