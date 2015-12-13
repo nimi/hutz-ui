7826df6067f99a7a258c3b2828bb26e8d4857586
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2015-12-13 11:15:14 -0500

    Create base for documentation nav

---
+++ example/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Box from '../../../src/Box';
+
+export default class Nav extends React.Component {
+
+	static displayName = 'Buttons'
+
+	render() {
+		return (
+			<nav>
+				<ul>
+					<li>Buttons</li>
+					<li>Inputs</li>
+				</ul>
+			</nav>
+		);
+	}
+}
