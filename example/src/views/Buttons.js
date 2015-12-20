6c9bf816863301d8119f7ef267f88e42f002a74a
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2015-12-20 09:44:06 -0500

    Add example of ButtonGroup component to docs

--- example/src/views/Buttons.js
+++ example/src/views/Buttons.js
@@ -1,52 +1,82 @@
 import React from 'react';
-import { Button } from '../../../src/components';
+import { Button, ButtonGroup, Box, FlexBox } from '../../../src/components';
+import jsxStringify from '../utils/jsxToString';
+import Highlight from 'react-highlight';
 
 export default class Buttons extends React.Component {
 
 	static displayName = 'Buttons'
+	static simpsons = ['Homer', 'Bart', 'Lisa', 'Marge', 'Maggie']
 
 	constructor() {
 		super();
 
-		this.state = {
-			status: 'primary',
-			disabled: false
-		};
+		this.state = {};
 	}
 
 	handleChange(event) {
 		this.setState( {status: event.target.value} );
 	}
 
-	render() {
-		let buttonProps, { status, disabled } = this.state;
+	randomSimpson() {
+		return Buttons.simpsons[Math.floor(Math.random() * Buttons.simpsons.length)];
+	}
 
-		let code = `<${ 'Button' }
-						status="${ status }"
-						disabled={${ disabled }}
-					/>`;
+	renderCode(Component) {
+		const code = jsxStringify(
+			Component
+		);
 
+		return <Highlight>{ code }</Highlight>
+	}
+
+	renderButton(props = {}) {
+		const buttonProps = { ...this.state, ...props };
+		return <Button { ...buttonProps }>
+			{this.randomSimpson()}
+		</Button>;
+	}
+
+	renderButtonGroup() {
 		return (
-			<div>
-				<h2>Buttons</h2>
-				<div style={{ display: 'flex', justifyContent: 'center' }}>
-					<div style={{ width: '25%' }}>
-						<label htmlFor="status">Status</label>
-						<select onChange={ this.handleChange }>
-							<option value="primary">Primary</option>
-							<option value="positive">Positive</option>
-							<option value="negative">Negative</option>
-						</select>
-					</div>
-					<div style={{ width: '50%' }}>
-						<Button {...buttonProps}>Button</Button>
-					</div>
-					<div style={{ width: '25%' }}>
-						<code>{ code }</code>
-					</div>
-				</div>
+			<ButtonGroup vertical>
+				{this.renderButton()}
+				{this.renderButton()}
+				{this.renderButton()}
+			</ButtonGroup>
+		);
+	}
 
-			</div>
+	renderComponentWithCode(component) {
+		return (
+			<FlexBox>
+				<Box col={ 6 } fill={ true } align='center'>
+					{component}
+				</Box>
+				<Box col={ 6 }>
+					{ this.renderCode(component) }
+				</Box>
+			</FlexBox>
+		);
+	}
+
+	render() {
+		return (
+			<FlexBox direction='column'>
+				<Box col={ 3 }>
+					<h2>Buttons</h2>
+				</Box>
+				<Box>
+					<h3 style={{ marginTop: 0, marginBottom: '2em' }}>Primary Button</h3>
+					{ this.renderComponentWithCode(
+						this.renderButton()
+					) }
+					<h3 style={{ marginTop: 0, marginBottom: '2em' }}>Button Group</h3>
+					{ this.renderComponentWithCode(
+						this.renderButtonGroup()
+					) }
+				</Box>
+			</FlexBox>
 		);
 	}
 }
