c83bb52232c43391e436ff72f22632079f5cf01d
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2015-12-31 15:54:18 -0500

    Add more style definitions to stateless select component

--- src/Select.js
+++ src/Select.js
@@ -1,4 +1,6 @@
 import React, { PropTypes } from 'react';
+import radium from 'radium'
+import { colors, typeography } from './styles';
 
 function Select(props) {
 	const opts = props.options.map((o, i) => {
@@ -8,11 +10,6 @@ function Select(props) {
 			</option>
 		);
 	});
-
-	const selectStyle = {
-		appearance: 'none',
-		WebkitAppearance: 'none'
-	};
 
 	return (
 
@@ -40,4 +37,17 @@ Select.propTypes = {
 	options: PropTypes.array.isRequired
 };
 
-export default Select;
+export default radium(Select);
+
+var selectStyle = {
+	backgroundColor: colors('gray', 1),
+	color: colors('gray', 5),
+	height: '2.5rem',
+	fontSize: typeography.body1.fontSize,
+	display: 'block',
+	verticalAlign: 'middle',
+	width: '100%',
+	':hover': {
+		backgroundColor: colors('gray', 2)
+	}
+};
