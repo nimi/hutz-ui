32e97c227deebe359b223aa8faf48d29f5a02832
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-02-14 12:04:02 -0500

    Add card component WIP

--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,33 @@ import React, { PropTypes } from 'react';
 import { colors } from './styles';
 import { Box, Container, Heading, Media, Text, Menu } from './Components';
 
-function CardActions({actions}) {
+function CardHeading({t, s}) {
 	return (
-		<Container py={3}>
+		<Container p={2}>
+			<Heading size={3} content={t} style={{margin: '0.25em 0'}} />
+			{(() =>
+				Boolean(s) ?
+				<Heading size={4} content={s} style={{margin: '0.5em 0'}} /> :
+				null
+			 )()}
+		</Container>
+	);
+}
+
+function CardMedia({s}) {
+	return <Container ><Media src={s} /></Container>;
+}
+
+function CardText({t}) {
+	return <Container px={2} py={1}><Text content={t} /></Container>;
+}
+
+function CardActions({as}) {
+	return (
+		<Container py={2}>
 			<Menu inline type={2} fill={false}>
 				{
-					actions.map(({action, name}, i) =>
+					as.map(({action, name}, i) =>
 						<a
 							onClick={action}
 							href='#'
@@ -34,25 +55,15 @@ function Card({
 	return (
 		<Box
 			{ ...props }
-			style={cardContainerStyle(props)}
+			style={{
+				...props.style,
+				...cardContainerStyle(props)
+			}}
 		>
-			<Container px={2}>
-				<Heading size={3} content={title} />
-				<Heading size={4} content={subtitle} />
-			</Container>
-			<Container>
-				<Media src={imgSrc} />
-			</Container>
-			<Container p={2}>
-				<Text content={text} />
-			</Container>
-			{
-				(() =>
-					actions ?
-					<CardActions actions={actions} /> :
-					null
-				)()
-			}
+			{(() => title ? <CardHeading t={title} s={subtitle} /> : null)()}
+			{(() => imgSrc ? <CardMedia s={imgSrc} /> : null)()}
+			{(() => text ? <CardText t={text} /> : null)()}
+			{(() => actions ? <CardActions as={actions} /> : null)()}
 		</Box>
 	);
 }
@@ -65,17 +76,6 @@ Card.propTypes = {
 	imgSrc: PropTypes.string,
 	text: PropTypes.string,
 	actions: PropTypes.array
-};
-
-Card.defaultProps = {
-	title: 'Card title',
-	subtitle: 'Card has subtitle',
-	imgSrc: 'http://www.adweek.com/files/imagecache/node-detail/news_article/hans-moleman-hed-2012.jpg',
-	text: 'Cards require a small amount of markup and classes to provide you with as much control as possible.',
-	actions: [
-		{ name: 'Link 1', action: () => console.log('clicky') },
-		{ name: 'Link 2', action: () => console.log('clicky') }
-	]
 };
 
 export default Card;
