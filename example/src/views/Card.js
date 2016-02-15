1e2836694fc7efbd485ba793dc84adfcb03d2c0e
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-02-14 19:14:25 -0500

    Add updated card docs

--- example/src/views/Card.js
+++ example/src/views/Card.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
-import {Card, Container, Heading} from '../../../src/components';
+import {Card, Container, FlexBox, Heading} from '../../../src/components';
 import {Example} from '../components';
 
 export default class CardView extends Component {
 
 	static displayName = 'CardView'
 
+	static defaultProps = {
+		title: 'Card title',
+		subtitle: 'Card has subtitle',
+		imgSrc: 'http://www.adweek.com/files/imagecache/node-detail/news_article/hans-moleman-hed-2012.jpg',
+		text: `Although seemingly elderly, Hans Moleman once claimed that he
+		   was only 31 years old, implying in the process that his appearance
+		   is due to heavy drinking and not a result of old age.`,
+		actions: [
+			{ name: 'Link 1', action: () => console.log('clicky') },
+			{ name: 'Link 2', action: () => console.log('clicky') }
+		]
+	}
+
+	getCardProps({title, subtitle, imgSrc, actions, text}) {
+		return {
+			title, subtitle, imgSrc, actions, text
+		};
+	}
+
 	render() {
+		const  props = this.getCardProps(this.props);
+
 		return (
 			<Container>
 				<Container>
@@ -16,7 +37,14 @@ export default class CardView extends Component {
 				</Container>
 				<Container>
 					<Example heading='Card'>
-						<Card />
+						<Card {...props} />
+					</Example>
+					<Example heading='Card'>
+						<FlexBox>
+							<Card {...props} style={{margin: '0 0.2em'}} />
+							<Card {...props} style={{margin: '0 0.2em'}} />
+							<Card {...props} style={{margin: '0 0.2em'}} />
+						</FlexBox>
 					</Example>
 				</Container>
 			</Container>
