98870b5c8c55c2db8d3a1ebf69a7a27ee15b27d0
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-04-15 17:15:46 -0400

    Add tabpanel component

--- src/components/TabPanel/index.js
+++ src/components/TabPanel/index.js
@@ -1,19 +1,37 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Children, cloneElement } from 'react';
+import TabPanelHeader from './TabPanelHeader';
 import Container from '../Container';
 import { colors } from '../../styles';
 
-function TabPanel({ ...props }) {
+function TabPanel({ activeIndex, backgroundColor, color, children, ...props }) {
 	const styles = Object.assign({},
 		style,
 		props.style
 	);
+
+	const activePanel = Children.map(children, (panel, i) => {
+		if (activeIndex === i) {
+			return cloneElement(panel, {
+				key: i,
+				...panel.props
+			});
+		}
+	}).filter(p => Boolean(p));
+
+	const headerProps = Children.map(children, ({props}) => props);
 
 	return (
 		<Container
 			style={styles}
 			{...props}
 		>
-			TabPanel
+			<TabPanelHeader
+				headerProps={headerProps}
+				activeIndex={activeIndex}
+				backgroundColor={backgroundColor}
+				color={color}
+			/>
+			{activePanel}
 		</Container>
 	);
 }
@@ -21,6 +39,7 @@ function TabPanel({ ...props }) {
 TabPanel.displayName = 'TabPanel';
 
 TabPanel.propTypes = {
+	activeIndex: PropTypes.number
 };
 
 export default TabPanel;
